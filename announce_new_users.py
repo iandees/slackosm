dfd0e34dfd5f6fbb259672cae18f10616c138059
Ian Dees <ian.dees@gmail.com>
2017-02-10 22:55:33 -0500

    Add new user announcer

---
+++ announce_new_users.py
@@ -0,0 +1,76 @@
+import arrow
+import logging
+import os
+import requests
+import redis
+import sys
+
+logger = logging.getLogger('osm')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+logger.addHandler(ch)
+redis_url = os.environ.get("REDIS_URL")
+r = redis.from_url(redis_url) if redis_url else dict()
+
+new_user_json_url = 'https://s3.amazonaws.com/data.openstreetmap.us/users/newest.json'
+slack_url = os.environ.get('SLACK_WEBHOOK_URL')
+
+
+def send_to_slack(message):
+    logger.info("Telling Slack: %s", message)
+    return requests.post(slack_url, json={'text': message})
+
+
+def interesting_change(feature):
+    props = feature.get('properties')
+    contains = props.get('inside')
+
+    if not contains:
+        return False
+
+    if filter(lambda c: c['wof:id'] == 85633793, contains):
+        return True
+    else:
+        return False
+
+
+features = requests.get(new_user_json_url).json()
+features = features['features']
+
+previous_timestamp = arrow.get(r.get('new_user_prev_timestamp'))
+logger.info("Checking for new users since %s", previous_timestamp)
+
+for feature in reversed(features):
+    props = feature.get('properties')
+    ts = arrow.get(props.get('timestamp'))
+    if not previous_timestamp or ts > previous_timestamp:
+        if not interesting_change(feature):
+            continue
+
+        inside = props.get('inside')
+        if inside:
+            contains = dict([(c['type'], c['name']) for c in inside])
+            locations = []
+            for t in ('locality', 'county', 'state', 'country'):
+                locations.append(contains.get(t))
+            locations = filter(None, locations)
+            location_str = ', '.join(locations)
+            location_str = ' in {}!'.format(location_str)
+        else:
+            location_str = '!'
+
+        send_to_slack(
+            u"`<https://www.openstreetmap.org/user/{}|{}>` just made "
+            "their <https://www.openstreetmap.org/changeset/{}|first edit>{}".format(
+                props.get('user').get('name'),
+                props.get('user').get('name'),
+                props.get('changeset').get('id'),
+                location_str,
+            )
+        )
+
+        previous_timestamp = ts
+
+logger.info("Done with new users, latest time %s", previous_timestamp.isoformat())
+r['new_user_prev_timestamp'] = previous_timestamp.isoformat()
